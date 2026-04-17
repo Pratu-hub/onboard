a07984676559e426305759076f506b794cc05341
Prathyush-RK <230701240@rajalakshmi.edu.in>
2026-04-17 15:59:25 +0530

    Corrected SWA URL in backend CORS to include .7. segment

--- backend/src/app.js
+++ backend/src/app.js
@@ -23,7 +23,7 @@ app.use(cors({
     'http://localhost:5174', 
     'http://localhost:5175',
     'https://blue-moss-09187bb00.2.azurestaticapps.net',
-    'https://happy-dune-073f9d400.azurestaticapps.net'
+    'https://happy-dune-073f9d400.7.azurestaticapps.net'
   ],
   credentials: true,
 }));
